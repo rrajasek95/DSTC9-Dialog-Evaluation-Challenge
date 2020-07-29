7aad84c95f37ba9ac4a38953e95702ca668712f6
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2020-07-29 09:29:08 -0700

    Add athena tag sampling

--- tc_anno_analysis.py
+++ tc_anno_analysis.py
@@ -38,24 +38,46 @@ def analyze_split(split_data):
     print("Switchboard DA:")
     analyze_da_information_for_split(da_mapping=switchboard_da_segment_mapping)
 
+def analyze_split_ath(split_data):
+    athena_da_segment_mapping = defaultdict(list)
 
+    for conv_id, conv_data in split_data.items():
+        turns = conv_data["content"]
+
+        for turn in turns:
+            segments = turn['segments']
+            athena_das = turn['athena_das']
+
+            for (segment, athena_da) in zip(segments, athena_das):
+                athena_da_segment_mapping[athena_da].append(segment)
+
+    print("Athena DA:")
+    analyze_da_information_for_split(athena_da_segment_mapping)
 
 def analyze_tc_data(args):
 
     splits = ['train' ] # , 'valid_freq', 'test_freq']
 
     for split in splits:
-        split_file = f'{split}_full_anno.json'
+        if args.ath :
+            split_file = f'{split}_anno_athena.json'
+        else:
+            split_file = f'{split}_full_anno.json'
         split_file_path = os.path.join(args.data_path, split_file)
 
         with open(split_file_path, 'r') as split_f:
             split_data = json.load(split_f)
         print("Split:", split)
-        analyze_split(split_data)
+
+        if args.ath:
+            analyze_split_ath(split_data)
+        else:
+            analyze_split(split_data)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', default='./tc_processed')
+    parser.add_argument('--ath', action='store_true')
     args = parser.parse_args()
     analyze_tc_data(args)
