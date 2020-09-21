750bf7d28c799d590432295a6e730a26340f8d92
Zach Tosh <ztosh@ucsc.edu>
2020-09-21 11:46:23 -0700

    modified cache

--- create_topical_chat_submission_dataset.py
+++ create_topical_chat_submission_dataset.py
@@ -72,31 +72,50 @@ def create_topical_chat_dict(args):
             reading_set.update(json.load(data_file))
     data = []
     cur_convo_id = keys[0]
-    turn_count = 0
+    turn_count = 1
     convo_history = []
-    for conv_id in keys:
-        if cur_convo_id != conv_id:
-            turn_count = 0
-            cur_convo_id = conv_id
-            convo_history = []
-        cur_convo = split_data[conv_id]
-        agent_knowledge, agent_mapping = utils.prepare_reading_set_for_conversation(conv_id, reading_set)
-        turn = cur_convo["content"][turn_count]
-        turn_knowledge = []
-        sentences = []
-        for segment in turn["segments"]:
-            sentence = segment["text"]
-            sentences.append(sentence)
-            segment_knowledge = prepare_sentence_knowledge_data(agent_mapping, conv_id, None, tokenizer,
-                                                                   turn, sentence, ranker)
-            turn_knowledge.append(segment_knowledge)
-        cur_turn_data = sentences, None, turn_knowledge
-        history_copy = deepcopy(convo_history)
-        data.append((history_copy, cur_turn_data))
-        history = sentences, None, None
-        convo_history.append(history)
-        turn_count += 1
-    torch.save(data, "valid_freq_cache")
+
+    cur_convo = split_data[cur_convo_id]
+    turn = cur_convo["content"][0]
+    sentences_first = []
+    for segment in turn["segments"]:
+        sentence = segment["text"]
+        sentences_first.append(sentence)
+    convo_history.append((sentences_first, None, None))
+
+    if args.create_knowledge:
+        create_knowledge_file(1, keys, cur_convo_id, split_data, reading_set, tokenizer, ranker)
+    else:
+        for conv_id in keys:
+            if cur_convo_id != conv_id:
+                turn_count = 1
+                cur_convo_id = conv_id
+                convo_history = []
+                cur_convo = split_data[cur_convo_id]
+                turn = cur_convo["content"][0]
+                sentences_first = []
+                for segment in turn["segments"]:
+                    sentence = segment["text"]
+                    sentences_first.append(sentence)
+                convo_history.append((sentences_first, None, None))
+            cur_convo = split_data[conv_id]
+            agent_knowledge, agent_mapping = utils.prepare_reading_set_for_conversation(conv_id, reading_set)
+            turn = cur_convo["content"][turn_count]
+            turn_knowledge = []
+            sentences = []
+            for segment in turn["segments"]:
+                sentence = segment["text"]
+                sentences.append(sentence)
+                segment_knowledge = prepare_sentence_knowledge_data(agent_mapping, conv_id, None, tokenizer,
+                                                                       turn, sentence, ranker)
+                turn_knowledge.append(segment_knowledge)
+            cur_turn_data = sentences, None, turn_knowledge
+            history_copy = deepcopy(convo_history)
+            data.append((history_copy, cur_turn_data))
+            history = sentences, None, None
+            convo_history.append(history)
+            turn_count += 1
+        torch.save(data, "valid_freq_cache")
 
 def clean(s):
     return ''.join([c if c not in string.punctuation else ' ' for c in s.lower()])
@@ -106,6 +125,34 @@ def prepare_sentence_knowledge_data(agent_mapping, conv_id, dialog_act, tokenize
     knowledge_sentence = ranker.get_top_fact(clean(sentence), conv_id, threshold=True)
     original_knowledge_sentence = agent_mapping[turn["agent"]].get(knowledge_sentence, "")
     return original_knowledge_sentence
+
+
+def create_knowledge_file(turn_count, keys, cur_convo_id, split_data, reading_set, tokenizer, ranker):
+    total_knowledge = []
+    for conv_id in keys:
+        if cur_convo_id != conv_id:
+            turn_count = 1
+            cur_convo_id = conv_id
+        cur_convo = split_data[conv_id]
+        agent_knowledge, agent_mapping = utils.prepare_reading_set_for_conversation(conv_id, reading_set)
+        turn = cur_convo["content"][turn_count]
+        turn_knowledge = []
+        for segment in turn["segments"]:
+            sentence = segment["text"]
+            segment_knowledge = prepare_sentence_knowledge_data(agent_mapping, conv_id, None, tokenizer,
+                                                                   turn, sentence, ranker)
+            turn_knowledge.append(segment_knowledge)
+        total_knowledge.append(turn_knowledge)
+        turn_count += 1
+    with open('valid_freq_facts_bert.txt', 'w') as f:
+        for item in total_knowledge:
+            fact_string = ""
+            for s in item:
+                if s == "":
+                    fact_string += "no_fact; "
+                else:
+                    fact_string += s + "; "
+            f.write("%s\n" % fact_string.replace("\n", "").strip())
 
 
 if __name__ == '__main__':
@@ -119,6 +166,8 @@ if __name__ == '__main__':
                         help="Path to knowledge index file")
     parser.add_argument('--model_metadata_path', type=str, default='./runs/bert_sentence_generation',
                         help='Path to the tokenizer and model configuration')
+    parser.add_argument('--create_knowledge', type=bool, default=False,
+                        help='generate just the knowledge')
     args = parser.parse_args()
     # create_topical_chat_dict(args)
 
